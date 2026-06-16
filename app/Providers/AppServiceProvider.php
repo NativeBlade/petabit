7029f659553250c119fa082df72327b419ebbbad
jeff <jeffleyd@gmail.com>
2026-06-16 17:19:53 +0200

    bump app version to 1.1.1 and bundle version to 1000010

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ use NativeBlade\Facades\NativeBladeConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
-    const VERSION = '1.1.0';
-    const BUNDLE_BERSION = 10;
+    const VERSION = '1.1.1';
+    const BUNDLE_BERSION = 1000010;
 
     public function register(): void
     {
